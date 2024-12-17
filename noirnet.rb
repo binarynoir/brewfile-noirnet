410d972ff0f4d7b88fe5e854cbc94d0f8f07b327
KryptonianSon <kryptonianson@users.noreply.github.com>
2024-12-17 09:11:23 -0500

    Update noirnet.rb

--- noirnet.rb
+++ noirnet.rb
@@ -1,8 +1,8 @@
 class Noirnet < Formula
   desc "Monitors to see if the device has internet access and send notifications"
   homepage "https://github.com/binarynoir/noirnet"
-  url "https://github.com/binarynoir/noirnet/archive/refs/tags/v1.3.1.tar.gz"
-  sha256 "1dd2d9e94c9a24fc2fa188b0f32e95da847d74deda50e853813cb4c08dbf1426"
+  url ""
+  sha256 ""
 
   depends_on "bash"
 
